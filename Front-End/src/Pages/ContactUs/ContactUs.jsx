a46e34cc5c85d8d810c53323c84d4af41339e329
fekiz12 <ekizfurkan2002@gmail.com>
2024-06-01 15:27:15 +0300

    furkan

---
+++ Front-End/src/Pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ContactUs.css'
+import Title from '../../Components/Title/Title'
+import Contact from '../../Components/Contact/Contact'
+const ContactUs = () => {
+  return (
+    <div>
+      <Title subTitle="CONTACT US" title="Get in Touch"/>
+      <Contact></Contact>
+    </div>
+  )
+}
+
+export default ContactUs
