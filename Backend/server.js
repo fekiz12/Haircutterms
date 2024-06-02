6e8f1b7e393cce35eb77ce763b2e830b847bbd43
fekiz12 <ekizfurkan2002@gmail.com>
2024-06-02 14:22:57 +0300

    furkan

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import "dotenv/config.js"
+import userRouter from "./routes/userRoute.js";
+
+//app config
+const app=express();
+const port=4000;    
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//db connection
+connectDB();
+
+//api endpoint
+app.use("/api/user",userRouter)
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`);
+})
