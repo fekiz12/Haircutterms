0ae25f1a3c79959bdd524ed86e6077a1b4fb221a
fekiz12 <ekizfurkan2002@gmail.com>
2024-06-05 20:23:21 +0300

    furkan

--- Backend/server.js
+++ Backend/server.js
@@ -11,9 +11,24 @@ import orderRouter from "./routes/orderRoute.js";
 const app=express();
 const port=4000;    
 
+const FRONTEND_URL = 'https://haircutterms.vercel.app';
+
+app.use(cors({
+  origin: FRONTEND_URL
+}));
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 //middleware
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 
 //db connection
 
