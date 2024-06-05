ebea3d7660b5e38a812063cc363a93e6a044132b
fekiz12 <ekizfurkan2002@gmail.com>
2024-06-05 21:54:05 +0300

    furkan

--- Front-End/src/Context/StoreContext.jsx
+++ Front-End/src/Context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://haircutterms-api.vercel.app";
     const [token, setToken] = useState("");
     const [urunList, setUrunList] = useState([]);
 
