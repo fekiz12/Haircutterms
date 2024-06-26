0d34436a1465a1a3edabf7904fff8e568ca929ff
fekiz12 <ekizfurkan2002@gmail.com>
2024-06-26 16:14:43 +0300

    furkan

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,50 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
-import "dotenv/config.js"
-import userRouter from "./routes/userRoute.js";
-import orderRouter from "./routes/orderRoute.js";
-import cartRouter from "./routes/cartRoute.js"
-import productRouter from "./routes/productRoute.js";
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
+import 'dotenv/config.js';
 
-//app config
-const app=express();
-const port=4000;    
+// App config
+const app = express();
+const FRONTEND_URL = 'https://haircutterms.vercel.app';
 
-//middleware
-app.use(express.json())
-app.use(cors())
+// CORS middleware
+const allowCrossDomain = function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, token');
+  next();
+};
 
-//db connection
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
+app.use(cors());
+
+// DB connection
 connectDB();
 
-//api endpoint
-app.use("/api/cart",cartRouter)
-app.use("/api/user",userRouter)
-app.use("/api/order",orderRouter)
-app.use("/images",express.static("uploads"));
-app.use("/api/product",productRouter);
+// API endpoints
+app.use('/api/product', productRouter);
+app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
 
+app.get('/', (req, res) => {
+  res.send('API Working');
+});
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
-
-app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port}`);
-})
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
